fb3f16e7621b818e4d39d0d2e6054af169225752
Safat765 <88162223+Safat765@users.noreply.github.com>
2025-03-25 00:20:08 +0600

    Final Assignment 2 SRMS

---
+++ srms_mvc/Data/alter.sql
@@ -0,0 +1,16 @@
+USE `assignment_srms`;
+
+ALTER TABLE course 
+MODIFY COLUMN status tinyint
+
+ALTER TABLE users 
+MODIFY COLUMN status tinyint
+
+ALTER TABLE users 
+MODIFY COLUMN user_type tinyint
+
+ALTER TABLE users 
+MODIFY COLUMN registration_number VARCHAR(20)
+
+ALTER TABLE exam 
+MODIFY COLUMN semester tinyint
